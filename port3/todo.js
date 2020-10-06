bb714d483dc0640dfa43606a90fcc1ea11100ba4
mingeon-kim <65947346+mingeon-kim@users.noreply.github.com>
2020-10-06 14:25:33 +0900

    a
    a

--- port3/todo.js
+++ port3/todo.js
@@ -7,6 +7,10 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+}
+
 function paintToDo(text) {
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
@@ -23,19 +27,26 @@ function paintToDo(text) {
         id : newId
     };
     toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value = '';
 }
+
 
 
 function loadToDos(){
     const lodedToDos = localStorage.getItem(TODOS_LS);
     if(lodedToDos !== null) {
-
+        const parsedToDos = JSON.parse(lodedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo,text);
+        });
+        
     }
 }
 
